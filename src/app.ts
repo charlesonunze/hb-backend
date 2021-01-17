275d990ca8d3cb1aa60c9cd48526d3ddfd3c821e
charlesonunze <charlesonunze2@gmail.com>
2021-01-17 19:15:10 +0100

    feat: add sentry for app monitoring

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
+
 import { PORT } from './config';
 import { logger } from './utils/main.logger';
 import { loadRoutes } from './startup/routes';
@@ -7,11 +9,18 @@ import { NotFoundErrorHandler, ServerErrorHandler } from './utils/errorHandler';
 
 const app = express();
 
+Sentry.init({
+	dsn:
+		'https://62dc4a4b5e674d119cc3590f49d95398@o361828.ingest.sentry.io/5596078'
+});
+
 // Load Middlewares
+app.use(Sentry.Handlers.requestHandler());
 loadMiddlewares(app);
 loadRoutes(app);
 
 // Handle Errors
+app.use(Sentry.Handlers.errorHandler());
 app.use(NotFoundErrorHandler);
 app.use(ServerErrorHandler);
 
